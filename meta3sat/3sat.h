832a89a5f7bfaff54a7ec22da8c606193abc6f1b
fofonjka <fofonjka@student.math.hr>
2012-12-11 10:23:08 +0100

    Update meta3sat/3sat.h

--- meta3sat/3sat.h
+++ meta3sat/3sat.h
@@ -1,5 +1,3 @@
-#include<cstdlib>
-#include<pthread.h>
 #include<iostream>
 #include<string>
 #include<vector>
@@ -7,58 +5,99 @@
 #include<fstream>
 #include<cmath>
 #include<map>
+#include<cstdio>
+#include"mtrand.h"
+#include<time.h>
 #include<set>
 using namespace std;
 
+
 class Zagrada;
 
-// KLASE
-
-bool manji(int a, int b)
-{
-	if(abs(a) <= abs(b)) return true;
-	else return false;
-}
-
-/*bool manja_zagrada(Zagrada A, Zagrada B)
-{
-	if( A.varijable.first() <= B.varijable.first() ) return true;
-	else return false;
-}*/
+extern vector<int> tezine;
+extern vector<int> pomocne_tezine;
+extern map<int, set<int> > veze_var_zagrada;
+extern vector<double> vjerojatnosti; 
+extern list<Zagrada> Formula;
 
 class Zagrada{
 public:
-	int id;
-	list<int> varijable;
-	int tezina;
-	
-	Zagrada(int a, int b, int c, int ID)
-	{
-		this->tezina = 1;
-		this->id = ID;
-		// odmah sortiramo listu
-		this->varijable.push_back(a);
-		this->varijable.push_back(b);
-		this->varijable.push_back(c);
-		this->varijable.sort(manji);
-	}	
+int id;
+list<int> varijable;
+int tezina;
+Zagrada(int a, int b, int c, int ID)
+{
+	this->tezina = 1;
+	this->id = ID;
+	// odmah sortiramo listu
+	this->varijable.push_back(a);
+	this->varijable.push_back(b);
+	this->varijable.push_back(c);
+}	
+};
+// LISTA FUNKCIJA
+
+// TODO Genetski_algoritam()
+
+
+
+// KLASE
+
+class Jedinka
+{
+public:
+	vector<bool> bitVektor;
+	double dobrota;
+
+	Jedinka(vector<int>& varijable , vector<double>& vjerojatnosti);
+	Jedinka(){}
+
+};
+
+class Populacija
+{
+public:
+	list<Jedinka> jedinke;
+	double ukupnaDobrota;
+
+	Populacija(){};
+	Populacija(int velicinaPopulacije , vector<int>& varijable, vector<double>& vjerojatnosti); //napravi konstruktor
+	void evaluirajPopulaciju(vector<int>& varijable , set<int>& zagrade); //treba napisati
 };
 
 
-// LISTA FUNKCIJA
-// funkcije za genetski algoritam
-vector<bool> genetski( int velicina_populacije , int broj_generacija , set<int>& varijable );
-set<int> pronadi_zagrade( set<int>& varijable );
-void azuriraj_vjerojatnosti( set<int>& varijable );
+vector<bool> genetski( int velicina_populacije , int broj_generacija , set<int>& varijable , double vjerojatnost_mutacije , double vjerojatnost_krizanja );
+
+set<int> pronadi_zagrade( vector<int>& varijable );
+
+void azuriraj_vjerojatnosti( vector<int>& varijable , vector<double>& vjerojatnosti, bool prviPut);
+
 double racunaj_dobrotu( vector<bool>& bitVektor , vector<int>& varijable , set<int>& zagrade );
+
 bool zadovoljena(int ID,vector<bool>& bitVektor,vector<int>& varijable);  //provjerava je li zagrada zadovoljena
-   // redukcija
-   // mutacija
-   // krizanje
-   // selekcija
-   // dobrota
-// TODO Racunaj_egzaktno()
-  //redukcija
-  //egzaktni racun
-//vector<int> Vrati_vektor_varijabli(list<Zagrada>::iterator i, list<Zagrada>::iterator j);
+
+void mutacija(Jedinka& j,double vjerojatnostMutacije , vector<double>& vjerojatnosti);
+
+void krizanje(Jedinka& prviRoditelj , Jedinka& drugiRoditelj , Jedinka& prvoDijete , Jedinka& drugoDijete , double vjerojatnostKrizanja,vector<double>& vjerojatnosti ,bool uniformno);
+
+void kopiraj_elitu(Populacija &p , Populacija &q , vector<int>& varijable , set<int>& zagrade , vector<double>& vjerojatnosti);
+
+Jedinka odaberiRoditelja(Populacija &p);
+
+bool po_dobroti(Jedinka first,Jedinka second);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
